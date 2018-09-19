78628e02edded2724e884eaedd2576218f57c498
Sreenath P Kyathanahally <pruthvi.iyengar@gmail.com>
2018-09-19 13:35:05 -0400

    Update Save_as_pickle.py

--- Script_to_run_in Rosenberg/Save_as_pickle.py
+++ Script_to_run_in Rosenberg/Save_as_pickle.py
@@ -2,24 +2,15 @@
 import pandas as pd
 import os
 
-
 #%%
-PATH = '/Users/pruthvi_local/Desktop/Lesion_Segmentation/01_preprocess/07Data_organized/'
+PATH = 'DATA_FOLDER' #% Data should be organized in this way -->Sub001/Cont01/Cont01.nii.gz; Sub001/Cont02/Cont02.nii.gz
 
 lst = os.listdir(PATH)
 if '.DS_Store' in lst:
     lst.remove('.DS_Store')
 
 n=len(lst)
-
 a="t2"
 contrast_lst=((((a)+'\n')*n)[:-1])
-
-#original_df = pd.DataFrame({"subject": lst})
-
-
 original_df = pd.DataFrame({"subject": lst, "contrast_foldname ": contrast_lst})
-
-original_df
-
-original_df.to_pickle("/Users/pruthvi_local/Desktop/Lesion_Segmentation/01_preprocess/subject_dict.pkl")
+original_df.to_pickle("/subject_dict.pkl") #% Path to save the pickle file
